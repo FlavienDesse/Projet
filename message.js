d290124fdf372a81d77b6e3ef924856b44858de8
MrFlavZz <flav621@gmail.com>
2017-05-01 15:52:04 +0200

    '
    '

---
+++ message.js
@@ -0,0 +1,423 @@
+var PixelsImage =[];
+
+var words = "extraordinaire chouette une cochon naturellement certainement circonstance commencement complètement connaissance conversation gouvernement intelligence parfaitement profondément accompagner appartement brusquement disparaître interrompre particulier recommencer reconnaître représenter transformer abandonner absolument apercevoir apparaître appartenir changement comprendre conscience considérer construire discussion distinguer entretenir envelopper expérience expression importance impossible impression inquiétude intéresser interroger magnifique maintenant nécessaire personnage poursuivre précipiter professeur rapidement recueillir rencontrer résistance ressembler révolution satisfaire secrétaire silencieux simplement souffrance surprendre surveiller tranquille travailler accomplir accrocher apparence apprendre approcher atteindre attention aujourhui autrefois autrement caractère cependant cinquante commander commencer compagnie compagnon condamner condition confiance confondre connaître consentir consulter contenter continuer contraire curiosité dangereux davantage découvrir descendre désespoir désormais différent difficile direction doucement également embrasser entraîner événement existence expliquer important inquiéter installer intention intérieur lendemain lentement longtemps maintenir militaire mouvement naissance parcourir permettre plusieurs politique poussière présenter président prétendre printemps promettre prononcer puissance rapporter recherche réfléchir réflexion regretter rejoindre remarquer remercier remplacer rencontre renverser repousser reprendre respecter retourner retrouver réveiller semblable sentiment seulement signifier situation souhaiter soumettre spectacle supérieur supporter tellement traverser troisième véritable vieillard accepter accorder admettre adresser affirmer ailleurs angoisse annoncer apporter arracher assister attacher attaquer attendre attitude aussitôt autorité aventure bataille beaucoup camarade campagne caresser chercher composer conclure conduire contenir convenir craindre déchirer déclarer défendre demander demeurer dépasser derrière désigner dessiner détacher détruire dimanche discours discuter disposer distance échapper éclairer éloigner empêcher employer emporter endormir enfermer enfoncer ensemble entendre entourer éprouver escalier éteindre étouffer étranger examiner exécuter exprimer fatiguer fauteuil fonction français franchir françois habiller habitant habitude histoire imaginer immobile importer indiquer inspirer instinct inventer jeunesse marchand meilleur mensonge ministre monsieur montagne nombreux observer occasion officier paraître partager parvenir paupière pénétrer personne plaindre poitrine position posséder possible pourquoi pourtant précéder précieux préférer préparer présence prévenir principe problème prochain produire profiter promener proposer protéger puissant quarante quartier question raconter ramasser rappeler rassurer recevoir réclamer regarder relation religion remettre remonter renoncer répandre répondre réserver résister résoudre respirer résultat retomber seigneur solitude souffler souffrir soulever soutenir souvenir supposer terminer terrible toujours trembler troubler vêtement violence vraiment abattre absence accuser acheter achever affaire ajouter allumer anglais appeler appuyer arrêter arrière arrivée arriver article asseoir assurer attirer avancer baisser bientôt bonheur branche briller cabinet capable certain cerveau chaleur chambre changer chanter charger chasser chemise chiffre choisir colline combien comment complet compter confier conseil content coucher couleur courage courant couvrir crainte creuser croiser cuisine curieux décider décrire dégager demande déposer dernier désirer devenir deviner diriger docteur dominer douceur douleur dresser écarter éclater écouter écraser effacer élément emmener émotion endroit énergie enfance engager enlever ensuite environ envoyer espérer essayer essuyer établir étendre étendue éternel étonner étrange étudier exemple exister exposer falloir famille fatigue fenêtre feuille figurer fortune frapper général glisser grandir habiter hauteur hésiter heureux honneur horizon ignorer immense imposer inconnu instant intérêt inutile inviter joindre journal journée justice liberté lorsque lumière machine maladie malheur manquer marcher mariage marquer matière mauvais médecin message mémoire menacer mériter million moindre montrer morceau musique naturel nerveux nourrir nouveau obliger obtenir occuper oreille oublier ouvrage parfois partout passage passion paysage pencher pendant plaisir planche pleurer plonger portier pousser pouvoir premier prendre présent presque presser prévoir profond prouver puisque qualité quelque quitter ramener rapport réalité reculer réduire refuser rejeter relever remplir rentrer répéter réponse reposer respect retenir retirer réussir révéler revenir risquer sauvage science seconde secours semaine sembler sentier séparer sérieux service silence société sommeil soudain sourire souvent suffire suivant surtout système tempête terrain terreur théâtre toucher tourner traîner traiter travail travers tromper trouver vaincre veiller victime village violent visible voiture volonté vouloir voyager absolu accent accord action agiter amener amuser ancien animal animer argent aspect auprès auquel autant auteur autour avance avenir avouer battre beauté besoin billet bouche briser brûler bureau cacher calmer casser causer centre cercle certes cesser chacun chaîne chaise chance chaque charge chasse chemin cheval cheveu claire classe colère combat commun compte contre couche couler couper courir course coûter croire danger danser autres debout défaut dehors demain départ depuis désert dessus détail devant devoir divers donner dormir double douter droite durant écrire effort élever empire encore enfant ennemi énorme entier entrée entrer épaule époque erreur espace espèce espoir esprit étaler étoile étroit éviter exiger facile faible faveur fermer fidèle figure flamme fonder forcer former gagner garçon garder gauche gloire goutte groupe guerre hasard humain humide jamais jardin jusque lequel lettre lisser livrer lutter madame maison maître malade malgré manger manier marché marche marier membre mentir mesure métier mettre milieu minute moitié moment monter mourir naître nation nature nombre nommer oeuvre offrir oiseau ouvert ouvrir palais papier paquet pareil parent parler parole partie partir passer patron pauvre paysan pendre pensée penser perdre peuple phrase pierre placer plaine plaire plante plutôt poésie pointe police porter précis prêter preuve prière prince prison projet propos propre public quatre quinze raison rapide regard rendre rester retour réunir revoir rideau rocher rompre rouler saisir saison saluer sauter sauver savoir second secret sentir serrer servir siècle signer simple social soirée soldat soleil sombre sommet songer sonner sortir source succès suivre taille tandis tantôt témoin tendre tenter tomber tracer trente trésor triste unique valeur valoir veille vendre ventre vérité verser visage vision visite vivant voisin voyage agent aider aimer ainsi aller alors amour année appel après arbre armée armer assez aucun aussi autre avant avoir bande barbe beaux blanc blond boire bruit calme carte cause céder celui cesse chair champ chant chaud chien choix chose chute clair coeur colon comme corde corps cours court créer crier crise croix cruel abord début désir digne doigt doute douze drame droit drôle durer éclat école effet enfin entre envie épais étage étude façon faire faute femme ferme fille finir fixer fleur folie force forêt forme foule frais franc frère froid front fruit fumée fumer fusil garde genou genre geste glace grâce grain grand grave guère haine haute herbe heure hiver homme honte hôtel image jambe jaune jeter jeune jouer juger juste large larme léger lever lèvre libre ligne livre lourd lueur lutte masse matin mêler mener mieux mille mince miser moins monde moyen bonjour noire notre nuage obéir objet odeur ombre oncle ordre parce parmi parti passé payer peine perdu perte peser petit pièce pitié place plein pluie poche poète poids point porte poser poste prier quand quant quart queue rayon règle repas reste rêver riche roche roman rouge route ruine sable saint salle salut santé scène selon seuil siège signe somme sorte sourd subir sueur suite sujet table tâche tache taire tapis temps tenir terme terre tirer titre toile tombe toute trace train trait trois usage vague vaste venir verre vêtir vieil vieux ville vingt vivre voici voilà voile voler votre abri acte afin agir aide aile arme avec avis banc beau bête bien bleu bois bord bout bras ceci cela cent chat chef cher chez ciel cinq clef coin côte côté coup cour dame dans déjà delà demi dent deux dieu dire donc dont doux eaux égal elle état être face faim fait faux fête fier fils fine fixe flot fois fond fort fuir gens goût gris gros haïr haut hier hors huit idée joie joli joue jour juge leur lien lier lieu lire loin long lors loup lune main mais mari même midi mien mine mode mois mort muet neuf noir nord note nous nuit oeil oser page pain paix papa part pays peau père peur pied plan plus pont port pour près prêt prix puis quel quoi race rang rare réel rêve rien rire robe rôle rond rose sais sang sans sein sens sept seul sien soin soir soit sous tant tard tête toit tour tout très trop trou tuer type user vent vers vert vide vite voie voir voix vous vrai yeux âgé âge air âme ami art bas bon but car cas cou cri dès dix dos dur eau est etc été fer feu fil fin foi fou ici île jeu lit loi lui mal mer moi mon mot mur nez nom non nul oui par pas peu pur que qui roi rue sac sec six soi sol son sou sûr sur tel toi ton tôt vie vif vin vol vue"  
+
+onmessage = function(event){
+
+PixelsImage=event.data
+go()
+};
+function go (){
+  var  rouge=""
+  var bleu=""
+  var vert=""
+  var opacite=""
+  
+
+   for (x=0 ; x<24 ; x=x+4) {
+    
+	var espace=0
+	
+    var composante_rouge=[]
+    var composante_vert=[]
+    var composante_bleu=[]
+    var composante_opacite=[]
+
+    
+    var message_composante_rouge=[]
+    var message_composante_rouge_decimal =[]
+
+    var message_composante_vert=[]
+    var message_composante_vert_decimal =[]
+
+    var message_composante_bleu=[]
+    var message_composante_bleu_decimal =[]
+
+    var message_composante_opacite=[]
+    var message_composante_opacite_decimal =[]
+
+    var mop_v = ""
+    var mop_b = ""
+    var mop_o = ""
+    var mop_r = ""
+    var mot_construit=""
+    
+    var  c=""
+    
+
+
+  // Ici on regarde le pixel de poid faible et on l'ajoute a un tableau
+  for (i=x;i<PixelsImage.length;i=i+4){
+    if(PixelsImage[i]%2==0){
+      composante_rouge.push(0)
+    }
+    else if (PixelsImage[i]%2==1){
+      composante_rouge.push(1)
+    }
+  }
+  for (d=0;d<composante_rouge.length; d=d+7){
+    for (a=d;a<7+d;a++){
+      c=c+composante_rouge[a]
+    }
+
+    message_composante_rouge_decimal.push(parseInt(c, 2))
+    c=""
+  }
+  // Les caractéres de notre alphabet en ASCII sont entre 64 et 91 et entre 96 et 123 et les autres caractéres sont les espaces , virgules ect
+  for (i=0;i<message_composante_rouge_decimal.length;i++){
+    if ((message_composante_rouge_decimal[i]>64 && message_composante_rouge_decimal[i]<91) || (message_composante_rouge_decimal[i]>96 && message_composante_rouge_decimal[i]<123)){
+  //La dés que c'est un caractére de notre alphabet ca le met dans message_composante_rouge
+  message_composante_rouge.push(message_composante_rouge_decimal[i])
+  espace=1
+}
+else {
+  if (espace == 1){
+    message_composante_rouge.push("32")
+  }
+  espace=0
+}
+}
+  // ici ca transforme les nombres en lettres
+  for (i=0;i<message_composante_rouge.length;i++) {
+    mop_r=mop_r + String.fromCharCode(message_composante_rouge[i])
+  }
+  rouge=rouge+mop_r
+
+
+
+
+
+
+  for (i=x;i<PixelsImage.length;i=i+4){
+    if(PixelsImage[i+1]%2==0){
+      composante_vert.push(0)
+    }
+    else if (PixelsImage[i+1]%2==1){
+      composante_vert.push(1)
+    }
+  }
+  // La on converti en decimal 
+
+
+  for (d=0;d<composante_vert.length; d=d+7){
+    for (a=d;a<7+d;a++){
+      c=c+composante_vert[a]
+    }
+
+    message_composante_vert_decimal.push(parseInt(c, 2))
+    c=""
+  }
+
+  // Les caractéres de notre alphabet en ASCII sont entre 64 et 91 et entre 96 et 123 et les autres caractéres sont les espaces , virgules ect
+  for (i=0;i<message_composante_vert_decimal.length;i++){
+  if ((message_composante_vert_decimal[i]>64 && message_composante_vert_decimal[i]<91) || (message_composante_vert_decimal[i]>96 && message_composante_vert_decimal[i]<123)){//La dés que c'est un caractére de notre alphabet ca le met dans message_composante_rouge
+    message_composante_vert.push(message_composante_vert_decimal[i])
+  espace=1
+}
+else {
+  if (espace == 1){
+    message_composante_vert.push("32")
+  }
+  espace=0
+}
+}
+for (i=0;i<message_composante_vert.length;i++) {
+  mop_v=mop_v + String.fromCharCode(message_composante_vert[i])
+}
+vert=vert+mop_v
+
+
+
+for (i=x;i<PixelsImage.length;i=i+4){
+  if(PixelsImage[i+2]%2==0){
+    composante_bleu.push(0)
+  }
+  else if (PixelsImage[i+2]%2==1){
+    composante_bleu.push(1)
+  }
+}
+for (d=0;d<composante_bleu.length; d=d+7){
+  for (a=d;a<7+d;a++){
+    c=c+composante_bleu[a]
+  }
+
+  message_composante_bleu_decimal.push(parseInt(c, 2))
+  c=""
+}
+  // Les caractéres de notre alphabet en ASCII sont entre 64 et 91 et entre 96 et 123 et les autres caractéres sont les espaces , virgules ect
+  for (i=0;i<message_composante_bleu_decimal.length;i++){
+  if ((message_composante_bleu_decimal[i]>64 && message_composante_bleu_decimal[i]<91) || (message_composante_bleu_decimal[i]>96 && message_composante_bleu_decimal[i]<123)){//La dés que c'est un caractére de notre alphabet ca le met dans message_composante_rouge
+    message_composante_bleu.push(message_composante_bleu_decimal[i])
+  espace=1
+}
+else {
+  if (espace == 1){
+    message_composante_bleu.push("32")
+  }
+  espace=0
+}
+}
+for (i=0;i<message_composante_bleu.length;i++) {
+  mop_b=mop_b + String.fromCharCode(message_composante_bleu[i])
+}
+bleu=bleu+mop_b
+
+
+
+
+for (i=x;i<PixelsImage.length;i=i+4){
+  if(PixelsImage[i+3]%2==0){
+    composante_opacite.push(0)
+  }
+  else if (PixelsImage[i+3]%2==1){
+    composante_opacite.push(1)
+  }
+}
+for (d=0;d<composante_opacite.length; d=d+7){
+  for (a=d;a<7+d;a++){
+    c=c+composante_opacite[a]
+  }
+
+  message_composante_opacite_decimal.push(parseInt(c, 2))
+  c=""
+}
+  // Les caractéres de notre alphabet en ASCII sont entre 64 et 91 et entre 96 et 123 et les autres caractéres sont les espaces , virgules ect
+  for (i=0;i<message_composante_opacite_decimal.length;i++){
+  if ((message_composante_opacite_decimal[i]>64 && message_composante_opacite_decimal[i]<91) || (message_composante_opacite_decimal[i]>96 && message_composante_opacite_decimal[i]<123)){//La dés que c'est un caractére de notre alphabet ca le met dans message_composante_rouge
+    message_composante_opacite.push(message_composante_opacite_decimal[i])
+  espace=1
+}
+else {
+  if (espace == 1){
+    message_composante_opacite.push("32")
+  }
+  espace=0
+}
+}
+for (i=0;i<message_composante_opacite.length;i++) {
+  mop_o=mop_o + String.fromCharCode(message_composante_opacite[i])
+}
+opacite=opacite+mop_o
+}
+
+lookForOccurrencesInsideText(words);
+
+function lookForOccurrencesInsideText (words) {
+
+   var values_b = []
+  var values_o = []
+  var values_r = []
+  var values_v = []
+
+
+  var text = opacite
+  var text = text.toLowerCase()
+  var mot_construit = []
+  var mot_ephemere =""
+  for (i=0 ; i<text.length ; i++) {
+
+    if (text.charAt(i) == " ") {
+      mot_construit.push(mot_ephemere)
+      mot_ephemere =""
+    }
+    else {
+      mot_ephemere = mot_ephemere + text.charAt(i) 
+    }
+    
+  }		
+
+
+  var count = 0
+  var pos = 0
+  var finit = "non"
+
+  for (let word of mot_construit) {
+    pos = words.indexOf(word)
+    while (count < 50 && finit!= "oui" ) {
+      
+     if ( words.charAt(word.length + pos)==" " && words.charAt(pos-1)==" " ) 
+     {
+       values_o.push(word);
+       finit = "oui"
+     }
+     
+     
+     count ++
+     
+     pos = words.indexOf(word, pos + 1);  
+     
+     
+   }
+   
+   count = 0
+   finit = "non"
+ }
+ 
+ 
+ 
+ 
+ 
+
+ var text = bleu
+ var text = text.toLowerCase()
+ var mot_construit = []
+ var mot_ephemere =""
+ for (i=0 ; i<text.length ; i++) {
+
+  if (text.charAt(i) == " ") {
+    mot_construit.push(mot_ephemere)
+    mot_ephemere =""
+  }
+  else {
+    mot_ephemere = mot_ephemere + text.charAt(i) 
+  }
+  
+}		
+
+
+var count = 0
+var pos = 0
+var finit = "non"
+
+for (let word of mot_construit) {
+  pos = words.indexOf(word)
+  while (count < 50 && finit!= "oui" ) {
+    
+   if ( words.charAt(word.length + pos)==" " && words.charAt(pos-1)==" " ) 
+   {
+     values_b.push(word);
+     finit = "oui"
+   }
+   
+   
+   count ++
+   
+   pos = words.indexOf(word, pos + 1);  
+   
+   
+ }
+ 
+ count = 0
+ finit = "non"
+}
+
+
+
+
+
+var text = vert
+var text = text.toLowerCase()
+var mot_construit = []
+var mot_ephemere =""
+for (i=0 ; i<text.length ; i++) {
+
+  if (text.charAt(i) == " ") {
+    mot_construit.push(mot_ephemere)
+    mot_ephemere =""
+  }
+  else {
+    mot_ephemere = mot_ephemere + text.charAt(i) 
+  }
+  
+}		
+
+
+var count = 0
+var pos = 0
+var finit = "non"
+
+for (let word of mot_construit) {
+  pos = words.indexOf(word)
+  while (count < 50 && finit!= "oui" ) {
+    
+   if ( words.charAt(word.length + pos)==" " && words.charAt(pos-1)==" " ) 
+   {
+     values_v.push(word);
+     finit = "oui"
+   }
+   
+   
+   count ++
+   
+   pos = words.indexOf(word, pos + 1);  
+   
+   
+ }
+ 
+ count = 0
+ finit = "non"
+}
+
+
+
+
+
+var text = rouge
+var text = text.toLowerCase()
+var mot_construit = []
+var mot_ephemere =""
+for (i=0 ; i<text.length ; i++) {
+
+  if (text.charAt(i) == " ") {
+    mot_construit.push(mot_ephemere)
+    mot_ephemere =""
+  }
+  else {
+    mot_ephemere = mot_ephemere + text.charAt(i) 
+  }
+  
+}		
+
+
+var count = 0
+var pos = 0
+var finit = "non"
+
+for (let word of mot_construit) {
+  pos = words.indexOf(word)
+  while (count < 50 && finit!= "oui" ) {
+    
+   if ( words.charAt(word.length + pos)==" " && words.charAt(pos-1)==" " ) 
+   {
+     values_r.push(word);
+     finit = "oui"
+   }
+   
+   
+   count ++
+   
+   pos = words.indexOf(word, pos + 1);  
+   
+   
+ }
+ 
+ count = 0
+ finit = "non"
+}
+
+
+
+
+
+
+
+postMessage(values_r);
+postMessage(rouge);
+postMessage(values_v);
+postMessage(vert);
+postMessage(values_b);
+postMessage(bleu);
+postMessage(values_o);
+postMessage(opacite);
+
+
+
+
+
+
+
+
+
+
+}
+}
